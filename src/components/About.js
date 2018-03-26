565b2884500e1eb8891f8a82339d03a3d8151b38
H4L0-Tahiti <35615840+H4L0-Tahiti@users.noreply.github.com>
2018-03-26 12:25:32 -1000

    1st review

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,43 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {withStyles} from 'material-ui/styles';
-import Style from '../decoration/Style'
-import {withGridCol} from '../decoration/withGridCol'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import Style from "../decoration/Style";
+import { withGridCol } from "../decoration/withGridCol";
 
-import Typography from 'material-ui/Typography/Typography';
-import Grid from 'material-ui/Grid';
-import {DialogActions, DialogContent, DialogContentText, DialogTitle} from 'material-ui/Dialog';
+import Typography from "material-ui/Typography/Typography";
+import Grid from "material-ui/Grid";
+import {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "material-ui/Dialog";
 
-const ABOUT_TEXT = "ceci est un text Ceic est un paragraphe \n Test est ce que Ca marche.Ptet "
-
+const ABOUT_TEXT = `
+ceci est un text Ceic est un paragraphe \n Test est ce que Ca marche.Ptet
+`;
+const TO_DO = `
+TODO:
+ - rapport : matière et date du cours
+- changement de mot de passe
+`;
 class About extends Component {
-
-    render() {
-        const {classes} = this.props;
-        return (
-            <div>
-                <Grid container direction="column" alignItems="stretch">
-                    <DialogTitle>A PROPOS</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText className={classes.textformat}>{ABOUT_TEXT}</DialogContentText>
-                    </DialogContent>
-                </Grid>
-            </div>
-        )
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Grid container direction="column" alignItems="stretch">
+          <DialogTitle>A PROPOS</DialogTitle>
+          <DialogContent>
+            <DialogContentText className={classes.textformat}>
+              {ABOUT_TEXT}
+              {TO_DO}
+            </DialogContentText>
+          </DialogContent>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default withStyles(Style)(About);
--- src/decoration/withGridCol.js
+++ src/decoration/withGridCol.js
@@ -1,17 +1,16 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Grid from 'material-ui/Grid';
+import Grid from "material-ui/Grid";
 
 export function withGridCol(comp) {
-    return class extends Component {
-        render() {
-            return (
-                <Grid container direction="column" alignItems="stretch">
-                   <comp {...this.props} />
-                </Grid>
-            )
-        }
+  return class extends Component {
+    render() {
+      return (
+        <Grid container direction="column" alignItems="stretch">
+          <comp {...this.props} />
+        </Grid>
+      );
     }
-
+  };
 }
